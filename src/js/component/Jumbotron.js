439a175e1c4d725f1d99fcd1dc34ddb3064564e1
EmersonAhumada <emerson.ahumada@gmail.com>
2021-09-28 08:23:14 +0000

    fix

--- src/js/component/Jumbotron.js
+++ src/js/component/Jumbotron.js
@@ -8,13 +8,8 @@ export function Jumbotron1() {
 				This is a simple hero unit, a simple jumbotron-style component
 				for calling extra attention to featured content or information.
 			</p>
-			<hr className="my-4" />
-			<p>
-				It uses utility classes for typography and spacing to space
-				content out within the larger container.
-			</p>
 			<a className="btn btn-primary btn-lg" href="#" role="button">
-				Learn more
+				Call to Action!
 			</a>
 		</div>
 	);
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,22 +8,26 @@ const Home = () => {
 		<div>
 			<Navbar />
 			<Jumbotron1></Jumbotron1>
+			<br></br>
 			<div className="row justify-content-center mx-4">
 				<Card1
 					imagen="https://plazamercado.shop/wp-content/uploads/2020/10/Pi%C3%B1a-Golden.jpg"
 					title="Piña"
 					text="Hola soy una Piña"
 				/>
+				<br></br>
 				<Card1
 					imagen="https://resizer.glanacion.com/resizer/3WDnqCk7XfarxJ3XXHy7vfCKeaE=/1119x746/smart/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/BJB5RPAYRNEOTK6D7JAHQ6ZCCA.jpg"
 					title="Banana"
 					text="Hola soy una Banana"
 				/>
+				<br></br>
 				<Card1
 					imagen="https://www.tododisca.com/wp-content/uploads/2020/11/Manzana-roja-y-verdes-1024x576.jpg"
 					title="Manzanas"
 					text="Hola, somos un par de Manzanas"
 				/>
+				<br></br>
 				<Card1
 					imagen="https://www.clikisalud.net/wp-content/uploads/2020/03/maneras-consumo-mango-protege-salud.jpg"
 					title="Mango"
